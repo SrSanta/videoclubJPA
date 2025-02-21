0da0a3a15aa4351e0ff7635f4d9cf6308bcbe098
Andres <andrescondegarcia11@gmail.com>
2025-02-21 14:43:21 +0100

    subida

---
+++ src/main/java/org/iesbelen/videoclub/controller/PeliculaNotFoundAdvice.java
@@ -0,0 +1,20 @@
+package org.iesbelen.videoclub.controller;
+
+import org.iesbelen.videoclub.exception.CategoriaNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class PeliculaNotFoundAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(CategoriaNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String peliculaNotFoundHandler(CategoriaNotFoundException CategoriaNotFoundException) {
+        return CategoriaNotFoundException.getMessage();
+    }
+
+}
